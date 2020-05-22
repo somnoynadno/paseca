4888b5a826545d44a45cc036356b52d40a053b25
somnoynadno <somnoynadno@yandex.ru>
2020-05-22 15:45:23 +0700

    Menu for custom types

--- src/pages/Preferences.js
+++ src/pages/Preferences.js
@@ -1,9 +1,12 @@
 import React from "react";
 import MainMenu from "../components/MainMenu"
-import {Button, Container, Grid, Image, Segment} from "semantic-ui-react";
+import {Button, Container, Grid, Image, Menu, Segment} from "semantic-ui-react";
 import {API} from "../http/API";
 
 import user from '../assets/user.png';
+import BeeFarmFamiliesTable from "../components/BeeFarmFamiliesTable";
+import BeeFarmHivesTable from "../components/BeeFarmHivesTable";
+import BeeFarmNotificationsTable from "../components/BeeFarmNotificationsTable";
 
 
 class Preferences extends React.Component {
@@ -11,8 +14,11 @@ class Preferences extends React.Component {
         super(props);
 
         this.state = {
-            user: null
+            user: null,
+            activeItem: null
         };
+
+        this.handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
         this.api = new API();
     }
@@ -64,6 +70,51 @@ class Preferences extends React.Component {
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
+                </Segment>
+                <Segment>
+                    <Grid columns={1}>
+                        <Grid.Row width={1}>
+                            <Grid.Column>
+                                <h1>Мои используемые типы</h1>
+                                <br />
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Menu stackable pointing secondary>
+                                    <Menu.Item
+                                        name='Породы пчёл'
+                                        active={this.state.activeItem === 'Породы пчёл'}
+                                        onClick={this.handleItemClick}
+                                    />
+                                    <Menu.Item
+                                        name='Сорта мёда'
+                                        active={this.state.activeItem === 'Сорта мёда'}
+                                        onClick={this.handleItemClick}
+                                    />
+                                    <Menu.Item
+                                        name='Болезни пчёл'
+                                        active={this.state.activeItem === 'Болезни пчёл'}
+                                        onClick={this.handleItemClick}
+                                    />
+                                    <Menu.Item
+                                        name='Форматы ульев'
+                                        active={this.state.activeItem === 'Форматы ульев'}
+                                        onClick={this.handleItemClick}
+                                    />
+                                    <Menu.Item
+                                        name='Рамки ульев'
+                                        active={this.state.activeItem === 'Рамки ульев'}
+                                        onClick={this.handleItemClick}
+                                    />
+                                </Menu>
+                            </Grid.Column>
+
+                            <Grid.Column>
+                                <Segment>
+                                    {/* TODO: render tables */}
+                                </Segment>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
                 </Segment>
             </Container>
         </div>
