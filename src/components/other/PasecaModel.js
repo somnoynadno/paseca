cf193f670a7922a2d068b377e15f43d796f6c5f5
somnoynadno <somnoynadno@yandex.ru>
2020-06-10 23:59:18 +0700

    Fixed bug (deleted family/hive) with PasecaModel

--- src/components/other/PasecaModel.js
+++ src/components/other/PasecaModel.js
@@ -73,8 +73,8 @@ class PasecaModel extends React.Component {
         if (this.state.beeFamilies === null || this.state.hives === null) {
             return <Segment style={{minHeight: "100px"}} loading />
         } else {
-            // это просто охуевшее дерьмо с трудоёмость O(n^4), но соре
             let rows = [];
+            // идём по всем координатам
             for (let i = 0; i < this.maxY; i++) {
                 let columns = [];
                 for (let j = 0; j < this.maxX; j++) {
@@ -82,30 +82,42 @@ class PasecaModel extends React.Component {
                     let action = <ChooseHiveForm coordX={j} coordY={i}
                                                  reloadCallback={this.reload.bind(this)}
                                                  beeFarmID={this.props.beeFarmID} /> ;
-                    // check for hive
+                    // проверяем ульи на сходство с координатой
                     for (let hive of this.state.hives) {
                         if (hive["coord_x"] === j && hive["coord_y"] === i) {
                             let pre = <span><strong>Улей: {hive.name}</strong>
                                 <br />Рамка: <u>{hive["hive_frame_type"].name}</u>
                                 <br />Формат: {hive["hive_format"].name + " (" + hive["hive_format"].size + ")"}
                                 <br /><br /></span>;
+                            // проверяем наличие семьи в улье
                             if (hive["bee_family_id"]) {
                                 action = <Button onClick={this.evictBeeFamily.bind(this, hive.id)}>
                                                 Выселить</Button>;
                                 let family = this.state.beeFamilies.find(family => family.id === hive["bee_family_id"]);
-                                color = family["bee_family_status"].color;
-                                pre = <span>{pre}
-                                    <strong>Семья: {family.name}</strong><br />
+                                if (family) {
+                                    // семья нашлась
+                                    color = family["bee_family_status"].color;
+                                    pre = <span>{pre}
+                                        <strong>Семья: {family.name}</strong><br />
                                     <i>Последний осмотр: <br />
-                                    {family["last_inspection_date"] ? (new Date(family["last_inspection_date"]))
-                                        .toLocaleString('ru', {
-                                        year: 'numeric',
-                                        month: 'long',
-                                        day: 'numeric',}) : '[нет данных]'}</i>
+                                        {family["last_inspection_date"] ? (new Date(family["last_inspection_date"]))
+                                            .toLocaleString('ru', {
+                                                year: 'numeric',
+                                                month: 'long',
+                                                day: 'numeric',}) : '[нет данных]'}</i>
                                     <br />
                                 </span>;
+                                } else {
+                                    // семья была удалена, улей пуст
+                                    color = "gray";
+                                    action = <div>
+                                        <ChooseBeeFamilyToHiveForm reloadCallback={this.reload.bind(this)} hiveID={hive.id} />
+                                        <Button onClick={this.removeHive.bind(this, hive.id)}>Убрать улей</Button>
+                                    </div>
+                                }
                             }
                              else {
+                                 // семьи никогда не было, улей пуст
                                 color = "gray";
                                 action = <div>
                                             <ChooseBeeFamilyToHiveForm reloadCallback={this.reload.bind(this)} hiveID={hive.id} />
@@ -123,6 +135,7 @@ class PasecaModel extends React.Component {
                 rows.push(<div key={i} className="row-style">{columns}</div>);
             }
 
+            // собираем схему пасеки
             return <div style={{overflowX: "scroll", overflowY: "scroll", marginTop: "5px"}}>
                 <div style={{width: this.maxX*12, height:this.maxY*12, boxSizing: "border-box"}}>
                     {rows}
